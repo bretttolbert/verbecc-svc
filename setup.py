9fbcaeb2f07d53ae52095fcf8e31a9e4d300fbff
Brett Tolbert <bretttolbert@gmail.com>
2017-07-08 13:15:34 -0500

    prep for publish

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import os
 from setuptools import setup, find_packages
 
 package = 'verb_conjugate_fr'
+package_dashed = package.replace('_', '-')
 setup_dir = os.path.dirname(os.path.abspath(__file__))
 version_file = os.path.join(setup_dir, package, 'VERSION')
 
@@ -17,10 +18,13 @@ required = [line for line in requirements if not line.startswith('-')]
 
 setup(name=package,
       version=version,
-      description='description',
-      url='https://github.com/bretttolbert/' + package.replace('_', '-'),
+      description='French Verb Conjugation Library',
+      url='https://github.com/bretttolbert/{}'.format(package_dashed),
+      download_url='https://github.com/bretttolbert/{}/archive/0.0.1.tar.gz'.format(package_dashed),
       author='Brett Tolbert',
       author_email='bretttolbert@gmail.com',
+      keywords=['french', 'verbs', 'conjugation', 'conjugator', 'language', 'linguistics'],
+      classifiers=[],
       packages=find_packages(),
       install_requires=[required],
       include_package_data=True,
