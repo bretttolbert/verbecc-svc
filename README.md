96492774824102b9a55ff111a40f09a987451864
Brett Tolbert <bretttolbert@gmail.com>
2019-06-07 12:37:31 -0500

    added views

--- README.md
+++ README.md
@@ -6,3 +6,47 @@ Un microservice Python avec un API REST pour la conjugaison des verbes français
 * Dockerized
 * Unit tested
 * Continuous Integration and Deployment with GitLab CI/CD
+
+Usage Examples
+
+
+http://localhost:8000/conjugate/mood/indicative/manger
+
+```
+{
+  "value": {
+    "present": [
+      "je mange",
+      "tu manges",
+      "il mange",
+      "nous mangeons",
+      "vous mangez",
+      "ils mangent"
+    ],
+    "imperfect": [
+      "je mangeais",
+      "tu mangeais",
+      "il mangeait",
+      "nous mangions",
+      "vous mangiez",
+      "ils mangeaient"
+    ],
+    "future": [
+      "je mangerai",
+      "tu mangeras",
+      "il mangera",
+      "nous mangerons",
+      "vous mangerez",
+      "ils mangeront"
+    ],
+    "simple-past": [
+      "je mangeai",
+      "tu mangeas",
+      "il mangea",
+      "nous mangeâmes",
+      "vous mangeâtes",
+      "ils mangèrent"
+    ]
+  }
+}
+```
--- python/verb_conjugate_fr/views.py
+++ python/verb_conjugate_fr/views.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 from verb_conjugate_fr import app
-from .conjugator import Conjugator
+from .conjugator import Conjugator, get_verb_stem
 from .conjugation_template import ConjugationTemplate
 from .conjugations_parser import ConjugationsParser
 from .verbs_parser import VerbsParser, VerbNotFoundError
+
+cg = Conjugator()
 
 @app.get("/")
 def read_root():
@@ -11,12 +13,23 @@ def read_root():
 
 @app.get("/conjugate/{infinitive}")
 def read_conjugation(infinitive: str):
-    conjugator = Conjugator()
-    out = conjugator.get_full_conjugation_string(infinitive)
+    out = cg.get_full_conjugation_string(infinitive)
     return {'value': out}
 
 @app.get("/find/infinitive/{infinitive}")
 def read_find_infinitive(infinitive: str):
-    verb_parser = VerbsParser()
-    verb = verb_parser.find_verb_by_infinitive(infinitive)
+    verb = cg.verb_parser.find_verb_by_infinitive(infinitive)
     return {'value': verb}
+
+@app.get("/find/conjugation-template/{template_name}")
+def read_find_conjugation_template(template_name: str):
+    template = cg.conj_parser.find_template(template_name)
+    return {'value': template}
+
+@app.get("/conjugate/mood/{mood}/{infinitive}")
+def read_conjugation_for_mood(mood: str, infinitive: str):
+    verb = cg.verb_parser.find_verb_by_infinitive(infinitive)
+    verb_stem = get_verb_stem(infinitive, verb.template)
+    template = cg.conj_parser.find_template(verb.template)
+    value = cg._get_full_conjugation_for_mood(verb_stem, template, mood)
+    return {'value': value}
