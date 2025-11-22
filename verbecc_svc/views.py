b2ddc4014a4d1d1e90d71cc741167fb0c7474e79
Brett Tolbert <bretttolbert@gmail.com>
2025-11-22 11:27:08 -0600

    wip

--- verbecc_svc/views.py
+++ verbecc_svc/views.py
@@ -32,12 +32,12 @@ def cg(lang):
 
 @app.get("/")
 def root():
-    return {"value": "Hello 世界"}
+    return_data("Hello 世界")
 
 
 @app.get("/supported-langs")
 def supported_langs():
-    return {"value": grammar_defines.SUPPORTED_LANGUAGES}
+    return return_data(grammar_defines.SUPPORTED_LANGUAGES)
 
 
 @app.get("/conjugate/{lang}/{infinitive}")
@@ -52,7 +52,7 @@ def conjugate(lang: str, infinitive: str, mood: str = "all", tense: str = "all")
             if tense != "all":
                 tc = cg(lang).conjugate_mood_tense(infinitive, Mood(mood), Tense(tense))
                 ret = tc
-        return ret.get_data()
+        return_data(ret.get_data())
     except Exception as exc:
         handle_error(exc)
 
@@ -60,7 +60,7 @@ def conjugate(lang: str, infinitive: str, mood: str = "all", tense: str = "all")
 @app.get("/search/infinitive/{lang}/{query}")
 def search_infinitive(lang: str, query: str, max_results=10):
     try:
-        return cg(lang).get_verbs_that_start_with(query, max_results)
+        return_data(cg(lang).get_verbs_that_start_with(query, max_results))
     except Exception as exc:
         handle_error(exc)
 
@@ -68,7 +68,7 @@ def search_infinitive(lang: str, query: str, max_results=10):
 @app.get("/find/infinitive/{lang}/{infinitive}")
 def find_infinitive(lang: str, infinitive: str):
     try:
-        return cg(lang).find_verb_by_infinitive(infinitive)
+        return_data(cg(lang).find_verb_by_infinitive(infinitive))
     except Exception as exc:
         handle_error(exc)
 
@@ -76,9 +76,13 @@ def find_infinitive(lang: str, infinitive: str):
 @app.get("/find/template/{lang}/{template}")
 def find_template(lang: str, template: str):
     try:
-        return cg(lang).find_template(template)
+        return_data(cg(lang).find_template(template))
     except Exception as exc:
         handle_error(exc)
+
+
+def return_data(value):
+    return {"value": value}
 
 
 def handle_error(exc):
@@ -100,4 +104,3 @@ def handle_error(exc):
             status_code=404,
             detail="Error: {}\nTraceback: {}".format(exval, traceback.format_tb(extb)),
         )
-    return {"value": value}
